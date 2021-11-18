e7ec179088a5193e186748182380a8b05a661406
takusan23 <takumu1200@gmail.com>
2021-11-19 02:44:29 +0900

    1.1.0だす

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "io.github.takusan23.photransfer"
         minSdk = 24
         targetSdk = 31
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
